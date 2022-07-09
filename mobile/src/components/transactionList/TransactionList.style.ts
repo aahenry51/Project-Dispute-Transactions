526b3cd334b29121f8c3867084cb9a7a283863c3
Your Name <you@example.com>
2022-07-09 06:08:23 -0400

    update Flatlist

---
+++ mobile/src/components/transactionList/TransactionList.style.ts
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+    backgroundColor: '#F5FCFF',
+  },
+  item: {
+    backgroundColor: 'lightgrey',
+    padding: 10,
+    width: '100%',
+    borderRadius: 2,
+
+    borderBottomWidth: 3,
+    borderBottomColor: 'white',
+  },
+  headerText: {
+    color: 'black',
+    marginLeft: 20,
+  },
+  subText: {
+    color: 'black',
+    marginLeft: 20,
+  },
+});
