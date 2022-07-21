d0cded964af05ab9642e5234c998211d991b5dda
Your Name <you@example.com>
2022-07-21 06:37:56 -0400

    form extend

--- mobile/src/screens/formScreen/FormScreen.style.ts
+++ mobile/src/screens/formScreen/FormScreen.style.ts
@@ -3,14 +3,23 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   input: {
     fontColor: 'black',
-  },
-
-  inputContainer: {
-    width: '90%',
-    marginTop: 50,
-    height: 150,
     backgroundColor: '#FFFFFF',
     borderColor: 'black',
     borderWidth: 2,
+    width: '90%',
+  },
+
+  inputContainer: {
+    marginTop: 50,
+
+    flexDirection: 'column',
+    alignItems: 'center',
+
+    height: 150,
+  },
+
+  button: {
+    width: '100%',
+    marginTop: 100,
   },
 });
--- mobile/src/screens/formScreen/FormScreen.view.tsx
+++ mobile/src/screens/formScreen/FormScreen.view.tsx
@@ -3,13 +3,28 @@ import {View, Text, Button, Alert, TextInput} from 'react-native';
 import {styles} from './FormScreen.style';
 import {TFormScreenViewProps} from './FormScreen.type';
 import {useRoute} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
 export const FormScreenView: FC<TFormScreenViewProps> = ({}) => {
   const route = useRoute();
-
+  const navigation = useNavigation();
   return (
-    <View style={styles.inputContainer}>
-      <TextInput style={styles.input} multiline={true} />
-    </View>
+    <>
+      <Text>{'Reason'}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Why??" />
+      </View>
+      <View style={styles.button}>
+        <Button
+          title={'Submit'}
+          onPress={() => {
+            navigation.reset({
+              index: 0,
+              routes: [{name: 'Transactions'}],
+            });
+          }}
+        />
+      </View>
+    </>
   );
 };
